3f0c3041c3b59ded2ea4a012df99db37709d4817
diegobarrutia <107280773+diegobarrutia@users.noreply.github.com>
2022-06-14 23:53:40 -0400

    Update equation.py
    Implemented numpy and preliminarily defined all the functions

--- equation.py
+++ equation.py
@@ -1,39 +1,66 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
-# Boltzmann constant (J/K)
-k = 1.380649 ** 0.00000000000000000000010
+
+" CONSTANTS "
+
+# Boltzmann constant (m2*kg*s-2*K-1)
+k = 1.380649 * 10 ** -23
 # h bar (J-s)
-h = 1.054571817 ** .0000000000000000000000000000000010
+h = 1.054571817 * 10 ** -34
 # temperature (K)
 T = 4
-# e
-e = 2.718281828459045
+# rho (kg/m)
+p = 1.67 * 10 ** -15
+# vs (km/s)
+v = 19.9
+# deformation potential (eV)
+Ds = 1.9228 * 10 ** -18
+# length of nanotube (m)
+L = 10 ** -10
+# confinement length (m)
+sigma = .5
+# omega from the bare energy of the Quantum Dots state
+omega_constant = 2
+
+
+" FUNCTIONS "
+
+# wave vector
+q = np.linspace(0.1, 0.5, num=10)
+
+# linear dispersion
+omega = v * q
+
+# deformation potential couplings
+G = Ds * q / np.sqrt(2 * p * L * h * omega)
+
+# form factor
+F = np.exp(-(q ** 2 * sigma)/4)
 
 # exciton coupling matrix elements (default values)
-g = np.random.randint(1, 10, size=(4, 6))
-
+g = G * F
 
 # linear dispersion (default values)
-w = np.random.randint(1, 10, size=(1, 6))
+w = v * q
 
 # dimensionless coupling strength
-gamma = np.divide(g, w)
+gamma = g/w
 
 # phonon occupation number
-n = []
+n = (np.exp((h * w)/(k * T)) - 1) ** -1
 
-for i in w[0]:
-    value = (e ** ((h * i)/(k * T)) - 1) ** -1
-    n.append(value)
+# time_dependent left_hand equation
+time_dependent = np.exp(((np.absolute(gamma)) ** 2) * (- n * np.absolute(np.exp(- omega * T) - 1)) ** 2)
 
+# time_independent left_hand equation
+time_independent = np.exp(((np.absolute(gamma)) ** 2) * np.exp(- omega * T) - 1)
 
-print(g)
-print(w)
-print(gamma)
-print(n)
+# phonon shifted transition frequency
+big_omega = omega_constant
+
+# linear susceptibility
+X = - np.exp(- omega_constant * T) * time_independent * time_independent
 
 # Here a sample on how to read a file
 # sample_data = pd.read_csv('sample_data.csv')
-
