505eb385515f9470387da6a136c3615c194dfdbd
diegobarrutia <107280773+diegobarrutia@users.noreply.github.com>
2022-06-24 10:22:23 -0400

    Update equation.py

--- equation.py
+++ equation.py
@@ -2,10 +2,11 @@ import numpy as np
 import warnings
 import matplotlib.pyplot as plt
 
-
 warnings.filterwarnings('ignore')
 
+
 " CONSTANTS "
+
 
 # move constant to a different files
 
@@ -14,7 +15,7 @@ k_b = 1.380649e-23
 # h bar (J-s)
 h_bar = 1.054571817e-34
 # temperature (K)
-T = 0.1
+T = 1
 # rho (kg/m)
 p = 1.67e-15
 # vs (m/s)
@@ -30,14 +31,15 @@ omega_constant = 1.27 * 1.60218e-19 / h_bar
 # Unknown constant from directly proportional relation
 C = 1
 
+
 " FUNCTIONS "
 
+
 # wave vector
-q = np.linspace(0.001, 0.01, num=500) * 1/L
+q = np.linspace(0.001, 0.01, num=1000) * 1/L
 
 # time (s)
-t = np.linspace(0, 3, num=500) * 1e-12
-
+t = np.linspace(0, 3, num=1000) * 1e-12
 # linear dispersion
 omega_s = v * q
 
@@ -59,8 +61,6 @@ gamma = g / w
 # phonon occupation number
 n = (np.exp((h_bar * w)/(k_b * T)) - 1) ** -1
 
-# phonon shifted transition frequency
-big_omega = omega_constant * h_bar - np.absolute(gamma) ** 2 @ omega_s
 
 # temperature_dependent
 temperature_dependent = 1j * np.exp(np.absolute(gamma) ** 2 * - n
@@ -82,14 +82,19 @@ plt.ylabel('X(t) - Temperature Independent')
 plt.xlabel('Time(s)')
 plt.show()
 
-# linear susceptibility
-linear_susceptibility = - 1j * np.exp(- 1j * big_omega * t) * C * temperature_independent * temperature_independent
+# phonon shifted transition frequency
+big_omega = omega_constant * h_bar - np.absolute(gamma) ** 2 @ omega_s
 
-plt.scatter(t, np.absolute(linear_susceptibility.imag))
-plt.yscale('log')
+# linear susceptibility
+linear_susceptibility = C * - 1j * np.exp(- 1j * big_omega * t) \
+                        * temperature_independent * temperature_independent
+
+plt.scatter(t, linear_susceptibility.imag)
 # limits according to the research paper
 plt.ylim(0.01, 1)
 plt.xlim(0, 2e-12)
+# logarithmic scale according to research paper
+plt.yscale('log')
 plt.title('Linear Susceptibility vs Time(s) ')
 plt.ylabel('X(t)')
 plt.xlabel('Time(s)')
