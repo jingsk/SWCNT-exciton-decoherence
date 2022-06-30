abfc3a70881f65783ce19b37f330dbb2b0b6b175
diegobarrutia <107280773+diegobarrutia@users.noreply.github.com>
2022-06-30 16:21:58 -0400

    Update equation.py
    Fixing of y-axis with Q instead of time adding the plotting code for the norm functions of ie^i*omega bar *t, Chi_o, Chi_T.

--- equation.py
+++ equation.py
@@ -15,7 +15,7 @@ k_b = 1.380649e-23
 # h bar (J-s)
 h_bar = 1.054571817e-34
 # temperature (K)
-T = 1
+T = 6
 # rho (kg/m)
 p = 1.67e-15
 # vs (m/s)
@@ -36,7 +36,7 @@ C = 1
 
 
 # wave vector
-q = np.linspace(0.001, 0.01, num=100) * 1/L
+q = np.linspace(0.001, 0.01, num=1000) * 1/L
 
 # time (s)
 t = np.linspace(0, 3, num=10000) * 1e-12
@@ -66,25 +66,51 @@ n_s = (np.exp((h_bar * w)/(k_b * T)) - 1) ** -1
 temperature_dependent = 1j * np.exp(np.absolute(gamma) ** 2 * - n_s
                                     @ (np.absolute(np.exp(- 1j * np.outer(omega_s, t)) - 1)) ** 2)
 
+# imaginary part
 plt.scatter(t, temperature_dependent.imag)
 plt.xlim(0, 2e-12)
 plt.yscale('log')
-plt.title('X_t vs Time(s)')
-plt.ylabel('X_t')
+plt.title('X_T vs Time(s) (Imaginary)')
+plt.ylabel('X_T')
 plt.xlabel('Time(s)')
 plt.show()
+
+# real part
+plt.scatter(t, temperature_dependent.real)
+plt.xlim(0, 2e-12)
+# plt.yscale('log')
+plt.title('X_T vs Time(s) (Real)')
+plt.ylabel('X_T')
+plt.xlabel('Time(s)')
+plt.show()
+
+# norm
+y1 = np.sqrt(temperature_dependent * np.conjugate(temperature_dependent))
 
 # temperature_independent
 temperature_independent = 1j * np.exp(np.absolute(gamma) ** 2
                                       @ (np.exp(- 1j * np.outer(omega_s, t)) - 1))
 
+# imaginary
 plt.scatter(t, temperature_independent.imag)
 plt.xlim(0, 2e-12)
 plt.yscale('log')
-plt.title('X_o vs Time (s)')
+plt.title('X_o vs Time (s) (Imaginary)')
 plt.ylabel('X_o')
 plt.xlabel('Time(s)')
 plt.show()
+
+# real
+plt.scatter(t, temperature_independent.real)
+plt.xlim(0, 2e-12)
+plt.yscale('log')
+plt.title('X_o vs Time (s) (Real)')
+plt.ylabel('X_o')
+plt.xlabel('Time(s)')
+plt.show()
+
+# norm
+y2 = np.sqrt(temperature_independent * np.conjugate(temperature_independent))
 
 # phonon shifted transition frequency
 big_omega = omega_constant * h_bar - np.absolute(gamma) ** 2 @ omega_s
@@ -108,42 +134,60 @@ plt.show()
 
 # Gamma
 plt.scatter(q, gamma)
-plt.xlim(0, 2e-12)
-plt.title('Gamma vs Time(s) ')
+plt.title('Gamma vs Q')
 plt.ylabel('Gamma')
-plt.xlabel('Time(s)')
+plt.xlabel('Q')
 plt.show()
 
 # n_s
 plt.scatter(q, n_s)
-plt.xlim(0, 2e-12)
-plt.title('n_s vs Time(s) ')
-plt.xlabel('Time(s)')
+plt.title('n_s vs Q')
+plt.xlabel('Q')
 plt.ylabel('n_s')
 plt.show()
 
 # np.absolute(np.exp(- 1j * omega_s * t) - 1) ** 2
-plt.scatter(t, (np.absolute(np.exp(- 1j * omega_s * t) - 1)) ** 2)
-plt.xlim(0, 2e-12)
-plt.title('np.absolute(np.exp(- 1j * omega_s * t) - 1) ** 2) (from X_T) vs Time(s) ')
-plt.xlabel('Time(s)')
+plt.scatter(q, (np.absolute(np.exp(- 1j * omega_s * t[0:1000]) - 1)) ** 2)
+plt.title('np.absolute(np.exp(- 1j * omega_s * t) - 1) ** 2) (from X_T) vs Q ')
+plt.xlabel('Q')
 plt.ylabel('np.exp(- 1j * omega_s * t) - 1')
 plt.show()
 
 # np.exp(- 1j * omega_s * t) - 1
-plt.scatter(t, (np.exp(- 1j * omega_s * t) - 1))
-plt.xlim(0, 2e-12)
-plt.title('np.exp(- 1j * np.outer(omega_s, t)) - 1) (from X_o) vs Time(s) ')
-plt.xlabel('Time(s)')
+plt.scatter(q, (np.exp(- 1j * omega_s * t[0:1000]) - 1))
+plt.title('np.exp(- 1j * np.outer(omega_s, t)) - 1) (from X_o) vs Q ')
+plt.xlabel('Q')
 plt.ylabel('np.exp(- 1j * omega_s * t) - 1')
 plt.show()
 
 # np.exp(- 1j * big_omega * t)
-plt.plot(t, np.exp(- 1j * big_omega * t))
-plt.xlim(0, .25e-12)
-plt.title('np.exp(- 1j * big_omega * t (from X) vs Time(s) ')
-plt.xlabel('Time(s)')
+
+# real
+plt.plot(q, np.exp(- 1j * big_omega * t[0:1000]).imag)
+plt.title('np.exp(- 1j * big_omega * t (from X) vs Q (Imaginary) ')
+plt.xlabel('Q')
 plt.ylabel('np.exp(- 1j * big_omega * t')
 plt.show()
+
+# imaginary
+plt.plot(q, np.exp(- 1j * big_omega * t[0:1000]).real)
+plt.title('np.exp(- 1j * big_omega * t (from X) vs Q (Real)')
+plt.xlabel('Q')
+plt.ylabel('np.exp(- 1j * big_omega * t')
+plt.show()
+
+# norm
+y3 = np.sqrt(np.exp(- 1j * big_omega * t) * np.conjugate(np.exp(- 1j * big_omega * t)))
+
+plt.plot(t, y1, label="y1")
+plt.plot(t, y2, label="y2")
+plt.plot(t, y3, label="y3")
+plt.yscale('log')
+plt.title('Norms of np.exp(- 1j * big_omega * t, X_T, and X_o')
+plt.ylabel('Functions')
+plt.xlabel('t')
+plt.legend()
+plt.show()
+
 
 
