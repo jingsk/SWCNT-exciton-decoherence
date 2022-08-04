d363c72025f5afb4fbe69562a3a860887539b657
diegobarrutia <107280773+diegobarrutia@users.noreply.github.com>
2022-08-04 12:07:07 -0400

    Update equation.py
    Replacing the equation after an analytical transformation from Sum to Integral

--- equation.py
+++ equation.py
@@ -1,213 +1,141 @@
 import numpy as np
 import warnings
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 warnings.filterwarnings('ignore')
 
-
 " CONSTANTS "
 
-
-# move constant to a different files
-
-# Boltzmann constant (J/K)
-k_b = 1.380649e-23
-# h bar (J-s)
-h_bar = 1.054571817e-34
-# temperature (K)
-T = 6
-# rho (kg/m)
-p = 1.67e-15
-# vs (m/s)
-v = 19900
-# deformation potential (J)
-Ds = 1.9228e-18
-# length of nanotube (m)
-L = 40e-10
-# confinement length (m)
-sigma = 13.5e-10
-# energy of the excited state (Hz)
-omega_constant = 0.2 * 1.60218e-19 / h_bar
-# Unknown constant from directly proportional relation
-C = 1
-
-
-" FUNCTIONS "
-
+# Boltzmann constant
+k_b = 1.380649e-23  # J/k
+# h bar
+h_bar = 1.054571817e-34  # Js
+# temperature
+T = 6  # K
+# linear mass density
+p = 1.67e-15  # kg/m
+# vs
+v = 19900  # m/s
+# deformation potential
+Ds = 2.243047191e-18  # J
+# length of nanotube
+L = 1.883e-9  # m
+# confinement length
+sigma = 3e-9  # m
+# energy of the excited state
+omega_constant = 1.41 * 1.60218e-19 / h_bar  # Hz
 
 # wave vector
-q = np.linspace(0.001, 0.01, num=1000) * 1/L
+q_array = (np.linspace(0, 0.2, num=1000) + .01) * 1 / L  # 1/m
 
-# time (s)
-t = np.linspace(0, 3, num=1000) * 1e-12
-# linear dispersion
-omega_s = v * q
+# time
+t_array = np.linspace(0, 3, num=1000) * 1e-12  # s
 
-# deformation potential couplings
-G = Ds * q / np.sqrt(2 * p * L * h_bar * omega_s)
-
-# form factor
-F = np.exp(-(q ** 2 * sigma ** 2)/4)
-
-# exciton coupling matrix elements
-g = G * F
-
-# linear dispersion (default values)
-w = v * q
 
 # dimensionless coupling strength
-gamma = g / w
+def gamma(q):
+    # deformation potential couplings
+    G = Ds * q / np.sqrt(2 * p * L * h_bar * v * q)
+    # form factor
+    F = np.exp(-(q ** 2 * sigma ** 2) / 4)
+    # linear dispersion
+    w = v * q
+    return G * F / w
+
 
 # phonon occupation number
-n_s = (np.exp((h_bar * w)/(k_b * T)) - 1) ** -1
+def n(q):
+    return (np.exp((h_bar * v * q) / (k_b * T)) - 1) ** -1
 
 
-# temperature_dependent
-temperature_dependent = 1j * np.exp(np.absolute(gamma) ** 2 * - n_s
-                                    @ (np.absolute(np.exp(- 1j * np.outer(omega_s, t)) - 1)) ** 2)
+# linear susceptibility temperature independent
+def X_o(q, t):
+    return gamma(q) ** 2 * (1 - np.cos(v * q * t))
 
-# imaginary part
-plt.scatter(t, temperature_dependent.imag)
-plt.xlim(0, 2e-12)
+
+exponents = - (L / np.pi) * np.array([quad(X_o, 0, np.pi / L, args=t)[0] for t in t_array])
+norm_X_o = np.exp(exponents)
+
+
+# linear susceptibility temperature dependent
+def X_T(q, t):
+    return n(q) * gamma(q) ** 2 * (2 - 2 * np.cos(v * q * t))
+
+
+exponents = - (L / np.pi) * np.array([quad(X_T, 0, np.pi / L, args=t)[0] for t in t_array])
+norm_X_T = np.exp(exponents)
+
+
+plt.plot(t_array, norm_X_o, t_array, norm_X_T)
+plt.title('|X_T| and |X_o| vs Time (ps)')
 plt.yscale('log')
-plt.title('X_T vs Time(s) (Imaginary)')
-plt.ylabel('X_T')
+plt.legend(['X_o', 'X_T'])
+plt.ylim([0.08, 1.1])
+plt.ylabel('|X_T| and |X_o|')
 plt.xlabel('Time(ps)')
 plt.show()
 
-# real part
-plt.scatter(t, temperature_dependent.real)
-plt.xlim(0, 2e-12)
-# plt.yscale('log')
-plt.title('X_T vs Time(s) (Real)')
-plt.ylabel('X_T')
-plt.xlabel('Time(ps)')
-plt.show()
-
-# norm
-y1 = np.sqrt(temperature_dependent * np.conjugate(temperature_dependent))
-
-# temperature_independent
-temperature_independent = 1j * np.exp(np.absolute(gamma) ** 2
-                                      @ (np.exp(- 1j * np.outer(omega_s, t)) - 1))
-
-# imaginary
-plt.scatter(t, temperature_independent.imag)
-plt.xlim(0, 2e-12)
-plt.yscale('log')
-plt.title('X_o vs Time (s) (Imaginary)')
-plt.ylabel('X_o')
-plt.xlabel('Time(ps)')
-plt.show()
-
-# real
-plt.scatter(t, temperature_independent.real)
-plt.xlim(0, 2e-12)
-plt.yscale('log')
-plt.title('X_o vs Time (s) (Real)')
-plt.ylabel('X_o')
-plt.xlabel('Time(ps)')
-plt.show()
-
-# norm
-y2 = np.sqrt(temperature_independent * np.conjugate(temperature_independent))
-
-# phonon shifted transition frequency
-big_omega = omega_constant * h_bar - np.absolute(gamma) ** 2 @ omega_s
 
 # linear susceptibility
-linear_susceptibility = C * - 1j * np.exp(- 1j * big_omega * t) \
-                        * temperature_independent * temperature_dependent
+def X(q, t):
+    return gamma(q) ** 2 * (1 + 2 * n(q)) * (1 - np.cos(v * q * t))
 
-plt.scatter(t, np.abs(linear_susceptibility.imag))
-# limits according to the research paper
-plt.xlim(0, 2e-12)
-# logarithmic scale according to research paper
+
+# high temperature
+T = 6  # K
+exponents = - (L / np.pi) * np.array([quad(X, 0, np.pi / L, args=t)[0] for t in t_array])
+norm1 = np.exp(exponents)
+
+# medium temperature
+T = 3  # K
+exponents = - (L / np.pi) * np.array([quad(X, 0, np.pi / L, args=t)[0] for t in t_array])
+norm2 = np.exp(exponents)
+
+# low temperature
+T = .01  # K
+exponents = - (L / np.pi) * np.array([quad(X, 0, np.pi / L, args=t)[0] for t in t_array])
+norm4 = np.exp(exponents)
+
+# exponential slope on logarithmic scale
+y = 1.4 * t_array ** .955
+
+# plotting
+plt.plot(t_array, norm1, t_array, norm2, t_array, norm4, t_array, y)
+plt.title('Linear Susceptibility (Research Paper) vs Time (ps)')
 plt.yscale('log')
-plt.title('|X(t)| vs Time(ps) ')
+plt.legend(['6 K', '3 K', '0.01 K'])
+plt.ylim([0.08, 1.1])
 plt.ylabel('|X(t)|')
 plt.xlabel('Time(ps)')
 plt.show()
 
+# (5,6) SWNTs chirality
 
-" Separation of Functions "
+sigma = 3.3e-10  # m
+T = 6  # K
 
-# Gamma
-plt.scatter(q, gamma)
-plt.title('Gamma vs Q')
-plt.ylabel('Gamma')
-plt.xlabel('Q')
-plt.show()
+exponents = - (L / np.pi) * np.array([quad(X, 0, np.pi / L, args=t)[0] for t in t_array])
+norm5 = np.exp(exponents)
 
-# n_s
-plt.scatter(q, n_s)
-plt.title('n_s vs Q')
-plt.xlabel('Q')
-plt.ylabel('n_s')
-plt.show()
+# medium temperature
+T = 3  # K
+exponents = - (L / np.pi) * np.array([quad(X, 0, np.pi / L, args=t)[0] for t in t_array])
+norm6 = np.exp(exponents)
 
-# np.absolute(np.exp(- 1j * omega_s * t) - 1) ** 2
-plt.scatter(q, (np.absolute(np.exp(- 1j * omega_s * t[0:1000]) - 1)) ** 2)
-plt.title('np.absolute(np.exp(- 1j * omega_s * t) - 1) ** 2) (from X_T) vs Q ')
-plt.xlabel('Q')
-plt.ylabel('np.exp(- 1j * omega_s * t) - 1')
-plt.show()
+# low temperature
+T = .01  # K
+exponents = - (L / np.pi) * np.array([quad(X, 0, np.pi / L, args=t)[0] for t in t_array])
+norm7 = np.exp(exponents)
 
-# np.exp(- 1j * omega_s * t) - 1
-plt.scatter(q, (np.exp(- 1j * omega_s * t[0:1000]) - 1))
-plt.title('np.exp(- 1j * np.outer(omega_s, t)) - 1) (from X_o) vs Q ')
-plt.xlabel('Q')
-plt.ylabel('np.exp(- 1j * omega_s * t) - 1')
-plt.show()
-
-# np.exp(- 1j * big_omega * t)
-
-# real
-plt.plot(q, np.exp(- 1j * big_omega * t[0:1000]).imag)
-plt.title('np.exp(- 1j * big_omega * t (from X) vs Q (Imaginary) ')
-plt.xlabel('Q')
-plt.ylabel('np.exp(- 1j * big_omega * t')
-plt.show()
-
-# imaginary
-plt.plot(q, np.exp(- 1j * big_omega * t[0:1000]).real)
-plt.title('np.exp(- 1j * big_omega * t (from X) vs Q (Real)')
-plt.xlabel('Q')
-plt.ylabel('np.exp(- 1j * big_omega * t')
-plt.show()
-
-# norm
-y3 = np.sqrt(np.exp(- 1j * big_omega * t) * np.conjugate(np.exp(- 1j * big_omega * t)))
-
-# plot of y1, y2, and y3
-plt.plot(t, y1, label="X_T")
-plt.plot(t, y2, label="X_o")
-plt.plot(t, y3, label="e^-i*omega bar *t")
+# plotting
+plt.plot(t_array, norm5, t_array, norm6, t_array, norm7, t_array, y)
+plt.title('Linear Susceptibility ((5,6) SWNTs) vs Time (ps)')
 plt.yscale('log')
-plt.title('Norms of np.exp(- 1j * big_omega * t, X_T, and X_o')
-plt.ylabel('Functions')
-plt.xlabel('t')
-plt.legend()
-plt.show()
-
-# norm of X_T * X_o
-
-y4 = y1 * y2
-plt.plot(t, y4, label="X_T * X_o")
-plt.yscale('log')
-plt.title('|X_T||X_o| vs t')
-plt.ylabel('Norm')
-plt.xlabel('t')
-plt.legend()
-plt.show()
-
-# oscillation in the norm
-
-y5 = np.sin((omega_s * t)/2) ** 2
-plt.plot(q, y5, label="sin((omega_s*t)/2)^2")
-plt.title('sin((omega_s*t)/2)^2 vs t')
-plt.xlabel('t')
-plt.legend()
+plt.legend(['6 K', '3 K', '0.01 K'])
+plt.ylim([0.08, 1.1])
+plt.ylabel('|X(t)|')
+plt.xlabel('Time(ps)')
 plt.show()
 
 
