766779fb3ff66aac912cbe8daf896b9c6eec0e99
jingsk <jingsk129@gmail.com>
2023-06-24 14:55:34 -0400

    rename phonon occupation number,n , to n_T to distinguish from swcnt's index

--- run_X.py
+++ run_X.py
@@ -23,18 +23,18 @@ def gamma(q):
     w = v_s * q
     return (G * F / w)
 #phonon occupation number
-def n(q):
+def n_T(q):
     return (np.exp((h_bar * v_s * q)/(K_b * T)) - 1) ** -1
 
 #the function to be integrated
 def X_integrand(q,t):
-    return gamma(q)**2 *(1+2*n(q)) * (1-np.cos(v_s * q *t))
+    return gamma(q)**2 *(1+2*n_T(q)) * (1-np.cos(v_s * q *t))
 
 def X0_integrand(q,t):
     return gamma(q)**2 * (1-np.cos(v_s * q *t))
 
 def XT_integrand(q,t):
-    return gamma(q)**2 *(2*n(q)) * (1-np.cos(v_s * q *t))
+    return gamma(q)**2 *(2*n_T(q)) * (1-np.cos(v_s * q *t))
 
 def norm(integ):
     exponents = -L / (1 * np.pi) * np.array([quad(integ, 0, 1*np.pi/L, args=(t))[0] for t in t_array])
@@ -57,11 +57,11 @@ if __name__ == "__main__":
 
     t_array2=t_array*1e12
     fig, ax = plt.subplots(figsize=(5,4))
-    ax.plot(t_array2,norms0_1[0], t_array2,norms0_1[1], t_array2,norms0_1[2])
+    ax.plot(t_array2,norms[0], t_array2,norms[1], t_array2,norms[2])
     #ax.axis('off')
-    axs.set_xlabel('Time (ps)')
-    plt.setp(axs, yscale='log', ylim=[0.08,1.1],xlim=[-0.01,2.5])
-    ax.xlabel('Time (ps)')
+    ax.set_xlabel('Time (ps)')
+    plt.setp(ax, yscale='log', ylim=[0.08,1.1],xlim=[-0.01,2.5])
+    #ax.xlabel('Time (ps)')
     ax.legend(['|X|','|X_T|','|X_0|'],fontsize=7.5)
     plt.tight_layout()
     plt.savefig('X.png',dpi=900)
