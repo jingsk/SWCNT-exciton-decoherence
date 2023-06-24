bc529bf467a1046fe87abd7802cfa11734c8bddf
jingsk <jingsk129@gmail.com>
2023-06-24 14:34:34 -0400

    wrote a swcnt class and create main called runX.py

---
+++ SWCNT.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+" CONSTANTS "
+# carbon-carbon bond length in aromatic systems 
+a = np.sqrt(3) * 1.44e-10  # m
+# mass of carbon atom
+mass_C = 1.99e-26  # kg
+h_bar = 1.054571817e-34 # Js
+eV2J = 1.60218e-19 # eV/J
+
+
+class SWCNT:
+    """
+    A class to represent a spin-active nuclear bath.
+    Takes in positions and randomly assigns isotopes.
+
+    Attributes
+    ----------
+    atoms: 
+        ase's Atoms object to get geometry
+    spin_table: 
+        contains atomic number, atomic mass, percent abundance, nuclear_spin
+        of species present in self.atoms
+    bath_geometry: 
+        contains atomic symbol, isotope, xyz positions, nuclear spin(I),
+        and the distance from e- Spin
+
+    Methods
+    -------
+    apply_rcutoff(e_spin_position, cutoff_radius):
+        remove sites beyond a certain radius from e- spin
+        use when e- spin has been defined
+    
+    Usage Guide
+    -------
+        SWCNT=bath.from_file('CONTCAR_NO2',(1,1,3))
+        SWCNT.bath_geometry
+        SWCNT.apply_r_cutoff(origin=[0,0,0], r=40)
+        SWCNT.bath_geometry
+    """
+    def __init__(SWCNT, n: int, m: int):
+        """
+        Structural properties of (n,m) SWCNT.
+
+        Parameters
+        ----------
+            n:
+                circumferntial folds. Pure (n,0) yields zigzag SWCNT.
+            m:
+                longtitudinal folds. m introduces chirality to zigzag SWCNTs.
+        """
+        self.n=n
+        self.m=m
+        #for unit cell length of SWCNT
+        self.L = get_unit_cell_length(self.n, self.m) #m
+        #get linear mass density
+        self.rho= get_n_atoms_per_cell(self.n,self.m) / self.L #Kg/m
+        #TODO: hook up to a database and make E11 and exciton localization class properties 
+        # confinement length (m)
+        self.sigma = 3e-9 #m #exciton
+        # energy of the excited state (Hz)
+        self.omega = 1.41 * eV2J / h_bar # Hz
+        # sound velocity in SWCNT (m/s)
+        # approx. as velocity in graphene (Xu, B. App. Phys. Lett., 96(18), 183108.)
+        self.v_s = 19900 # m/s
+        # deformation potential (J)
+        # approx. as D in graphene (Yu, J. JCP, 103(15), 6697–6705.)
+        self.D_s = 2.243047191e-18 # J
+
+
+    def get_unit_cell_length(n: int, m: int):
+        """
+        for present (n,m) use a formula to give unit cell length
+        
+        Parameters
+        ----------
+                n:
+                    circumferntial folds. Pure (n,0) yields zigzag SWCNT.
+                m:
+                    longtitudinal folds. m introduces chirality to zigzag SWCNTs.
+        """
+        return 3 * a / (get_dr(n,m)) * np.sqrt(n**2 + n*m + m**2) #m
+
+
+    def get_n_atoms_per_cell(n: int, m: int):
+        """
+        for present (n,m) use a formula to give number of atoms per cell
+        
+        Parameters
+        ----------
+                n:
+                    circumferntial folds. Pure (n,0) yields zigzag SWCNT.
+                m:
+                    longtitudinal folds. m introduces chirality to zigzag SWCNTs.
+        """
+        return 4* np.sqrt(n**2 + n*m + m**2) / get_dr(n,m) #Kg/cell
+
+        
+    def get_dr(n: int, m: int):
+        """
+        for L, rho calculations. d_r defines redundancy along SWCNT vector
+        
+        Parameters
+        ----------
+                n:
+                    circumferntial folds. Pure (n,0) yields zigzag SWCNT.
+                m:
+                    longtitudinal folds. m introduces chirality to zigzag SWCNTs.
+        """
+        return np.gcd(2*m + n, 2*n + m)  #unitless
+    
+        #provided user input set exciton confinement
+    def set_sigma(self,sigma)
+        self.sigma=sigma
+
+    #provided user input set first excited state
+    def set_omega(self,omega)
+        self.omega=omega
---
+++ run_X.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from SWCNT import SWCNT
+
+" CONSTANTS "
+
+# Boltzmann constant (J/K)
+K_b = 1.380649e-23
+# planck's constant
+h_bar = 1.054571817e-34 #Js
+
+#time to evaluate Chi
+t_array = np.linspace(0, 3, num=100) * 1e-12 #s
+
+
+
+
+#unitless vibrational coupling
+def gamma(q,swcnt):
+    G = swcnt.D_s * q / np.sqrt(2 * rho * L * h_bar * v_s * q)
+    F = np.exp(-(q ** 2 * sigma ** 2)/4)
+    w = swcnt.v_s * q
+    return (G * F / w)
+#phonon occupation number
+def n(q):
+    return (np.exp((h_bar * v_s * q)/(K_b * T)) - 1) ** -1
+
+#the function to be integrated
+def X_integrand(q,t):
+    return gamma(q)**2 *(1+2*n(q)) * (1-np.cos(v_s * q *t))
+
+def X0_integrand(q,t):
+    return gamma(q)**2 * (1-np.cos(v_s * q *t))
+
+def XT_integrand(q,t):
+    return gamma(q)**2 *(2*n(q)) * (1-np.cos(v_s * q *t))
+
+def norm(integ):
+    exponents = -L / (1 * np.pi) * np.array([quad(integ, 0, 1*np.pi/L, args=(t))[0] for t in t_array])
+    norm = np.exp(exponents)
+    return norm
+
+if __name__ == "__main__":
+    n,m = 6,4
+    swcnt = SWCNT(n,m)
+    L = swcnt.L
+    sigma = swcnt.L
+    rho = swcnt.rho
+    v_s = swcnt.v_s
+    D_s = swcnt.D_s
+    #time to evaluate Chi
+    t_array = np.linspace(0, 3, num=100) * 1e-12 #s
+    #simulation temperature in K
+    T = 4 #K
+    norms = [norm(X_integrand),norm(XT_integrand), norm(X0_integrand)]
+
+    t_array2=t_array*1e12
+    fig, ax = plt.subplots(figsize=(5,4))
+    ax.plot(t_array2,norms0_1[0], t_array2,norms0_1[1], t_array2,norms0_1[2])
+    #ax.axis('off')
+    axs.set_xlabel('Time (ps)')
+    plt.setp(axs, yscale='log', ylim=[0.08,1.1],xlim=[-0.01,2.5])
+    ax.xlabel('Time (ps)')
+    ax.legend(['|X|','|X_T|','|X_0|'],fontsize=7.5)
+    plt.tight_layout()
+    plt.savefig('X.png',dpi=900)
